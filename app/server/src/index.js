aad2d954d28e51927a6022c20c4a8bc4b379e6ba
Uladzimir Kuzmin <specialkv@gmail.com>
2018-10-11 00:49:48 +0300

    updated docker-compose, removed .env

--- app/server/src/index.js
+++ app/server/src/index.js
@@ -1,17 +1,3 @@
-import Koa from 'koa';
-import logger from 'koa-morgan';
-import env from 'dotenv';
-
-env.config();
-
-const server = new Koa();
-
-server
-  .use(logger('tiny'))
-  .use(async ctx => {
-    ctx.body = 'Hello World!';
-  })
-  .listen(process.env.PORT, () => {
-    console.log(`server start listening on port ${process.env.PORT}`);
-  });
+require('@babel/register');
+require('./server.js');
   
