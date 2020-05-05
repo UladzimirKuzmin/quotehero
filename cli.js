c58bde52db338bb32b70c8e48648835e540fc9e2
Uladzimir Kuzmin <vladimir.kuzmin@effective-soft.com>
2020-05-05 22:27:39 +0300

    updated deps & cli changes

--- cli.js
+++ cli.js
@@ -7,19 +7,30 @@ import options from './options';
 
 const readDirAsync = promisify(readdir);
 
-const initializeGenerator = (image, index=0, src='./images') => {
+/**
+ * Initializes generator
+ *
+ * @param {string} original
+ * @param {number} index
+ * @param {string} src
+ * @param {string} dist
+ *
+ * @returns {Object}
+ */
+const initializeGenerator = (original, index=0, src='./images', dist='./dist') => {
   return new Generator(Object.assign({}, options[index], {
     id: `generator_${index + 1}`,
-    src: src,
-    original: image,
-    result: `image${index + 1}${path.extname(image)}`,
+    original,
+    src,
+    dist,
+    result: `image${index + 1}${path.extname(original)}`,
     cb: onImageGenerated,
   }));
 }
 
 const onImageGenerated = function(err) {
   if (err) return console.dir(arguments);
-  console.log(this.outname + " created  ::  " + arguments[3]);
+  console.log(this.outname + ' created  ::  ' + arguments[3]);
 };
 
 const getImages = async (src='./images') => {
@@ -34,7 +45,8 @@ const getImages = async (src='./images') => {
 
 export const generateQuote = async () => {
   try {
-    initializeGenerator(argv.filename).generate();
+    const { filename, src, dist } = argv
+    initializeGenerator(filename, undefined, src, dist).generate();
   } catch(err) {
     console.log(err);
   }
@@ -42,9 +54,10 @@ export const generateQuote = async () => {
 
 export const generateQuotes = async () => {
   try {
-    const images = await getImages(argv.src);
+    const { src, dist } = argv
+    const images = await getImages(src);
     images.forEach((image, index) => {
-      initializeGenerator(image, index, argv.src).generate();
+      initializeGenerator(image, index, src, dist).generate();
     });
   } catch(err) {
     console.log(err);
